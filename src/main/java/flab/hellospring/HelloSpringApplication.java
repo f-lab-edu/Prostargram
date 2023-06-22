41837c2308d123f460c11ddc82ba17f5567ab9ca
eunbileeme <pask2202@gmail.com>
2023-06-22 23:29:31 +0900

    initial commit

---
+++ src/main/java/flab/hellospring/HelloSpringApplication.java
@@ -0,0 +1,13 @@
+package flab.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
