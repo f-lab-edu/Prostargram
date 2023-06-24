54ccf1819a480e357e2512f271f8a36c75973930
Ayaan <alsdnrdl001@gmail.com>
2023-06-25 00:03:38 +0900

    [feat] testAPI 추가

---
+++ src/main/java/flab/project/controller/TestController.java
@@ -0,0 +1,15 @@
+package flab.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    // 서버가 동작은 하지만, 내가 작성한 API가 작동을 안하는지 확인할 때 사용
+    @GetMapping("/test")
+    public String Test() {
+        return "test";
+    }
+
+}
