7fdc756c4cab48f4417c0522edfdd996c7c3278f
Ayaan <alsdnrdl001@gmail.com>
2023-06-24 23:58:55 +0900

    [chore] project명 변경

--- src/main/java/flab/project/ProjectApplication.java
+++ src/main/java/flab/project/ProjectApplication.java
@@ -1,13 +1,13 @@
-package flab.hellospring;
+package flab.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSpringApplication {
+public class ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 }
