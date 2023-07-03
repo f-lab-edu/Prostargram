8f5cc46570db184b6787ef236fe0785cee713d62
Ayaan <alsdnrdl001@gmail.com>
2023-07-03 14:36:47 +0900

    [feat] BaseResponse 설계 - BaseResponse 클래스를 설계하였다. - BaseResponseStatus Enum을 구현하였다.

---
+++ src/main/java/flab/project/config/baseresponse/BaseResponse.java
@@ -0,0 +1,45 @@
+package flab.project.config.baseresponse;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static flab.project.config.baseresponse.BaseResponseStatus.SUCCESS;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class BaseResponse<T> {
+
+    @Schema(name = "성공 여부", example = "true")
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+
+    @Schema(name = "요청에 대한 결과 메세지", example = "요청에 성공하였습니다.")
+    private final String message;
+
+    @Schema(name = "요청에 대한 결과 코드", example = "1000")
+    private final int code;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    // 요청에 성공한 경우
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.message = SUCCESS.getMessage();
+        this.code = SUCCESS.getCode();
+        this.result = result;
+    }
+
+    // 요청에 실패한 경우
+    public BaseResponse(BaseResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+    }
+
+}
