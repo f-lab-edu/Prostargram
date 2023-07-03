dd0584fe6e8046fa6ce478c603c8fe2820d72e64
Ayaan <alsdnrdl001@gmail.com>
2023-07-03 14:55:46 +0900

    [feat] BaseResponse 클래스 설계 - BaseResponse 클래스 설계 - BaseResponseStatus enum 작성

---
+++ src/main/java/flab/project/config/baseresponse/BaseResponseStatus.java
@@ -0,0 +1,27 @@
+package flab.project.config.baseresponse;
+
+import lombok.Getter;
+
+/**
+ * 에러 코드 관리
+ */
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다.");
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
+
+
