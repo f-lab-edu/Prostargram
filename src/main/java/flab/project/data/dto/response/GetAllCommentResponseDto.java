e1ad34f6e6cd1cd9f6bb73ece529122b0a24c4f5
eunbileeme <pask2202@gmail.com>
2023-07-12 13:54:26 +0900

    [feat] GetAllCommentResponseDto 설계 중

---
+++ src/main/java/flab/project/data/dto/response/GetAllCommentResponseDto.java
@@ -0,0 +1,12 @@
+package flab.project.data.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Schema(description = "댓글을 가져오는 응답 Dto")
+public class GetAllCommentResponseDto {
+    // 대댓글을 구현하기 위한 상속 관계 설계 후, 구현 예정
+}
