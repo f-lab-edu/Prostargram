c41d289a01f0f686fd80da5e0dcc869bcf5ab920
eunbileeme <pask2202@gmail.com>
2023-07-15 01:11:28 +0900

    [refactor] (상속을 구현할 경우) CreatePostResponseDto 설계

--- src/main/java/flab/project/data/dto/response/CreatePostResponseDto.java
+++ src/main/java/flab/project/data/dto/response/CreatePostResponseDto.java
@@ -1,5 +1,6 @@
 package flab.project.data.dto.response;
 
+import flab.project.data.dto.model.BasicPostInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -7,23 +8,10 @@ import java.util.List;
 
 @Getter
 @Schema(description = "피드 작성 결과를 나타내는 DTO")
-public class CreatePostResponseDto {
+public class CreatePostResponseDto extends BasicPostInfo {
 
 	@Schema(example = "1")
 	private final long postId;
-
-	// Long Polling 방식을 사용하므로 request의 데이터 = response의 데이터
-	@Schema(example = "1")
-	private final long userId;
-
-	@Schema(example = "이은비")
-	private final String userName;
-
-	@Schema(example = "https://profileImg.url")
-	private final String profileImgUrl;
-
-	@Schema(example = "오늘 깃허브에 알고리즘 관련 내용을 정리했습니다.", maxLength = 100)
-	private final String content;
 
 	@Schema(example = "https://imageUrl.url", nullable = false)
 	private final List<String> contentImgUrls;
@@ -38,19 +26,11 @@ public class CreatePostResponseDto {
 	@Schema(example = "방금 전")
 	private final String creadtedAt;
 
-	@Schema(example = "java")
-	private final List<String> hashTags;
-
-	public CreatePostResponseDto(long userId, String userName, String profileImgUrl, String content, List<String> contentImgUrls, String creadtedAt, List<String> hashTags, long postId, long likeCounts, long commentCounts) {
+	public CreatePostResponseDto(List<String> contentImgUrls, String creadtedAt, long postId, long likeCounts, long commentCounts) {
 		this.postId = postId;
-		this.userId = userId;
-		this.userName = userName;
-		this.profileImgUrl = profileImgUrl;
-		this.content = content;
 		this.contentImgUrls = contentImgUrls;
 		this.creadtedAt = creadtedAt;
 		this.likeCounts = likeCounts;
 		this.commentCounts = commentCounts;
-		this.hashTags = hashTags;
 	}
 }
