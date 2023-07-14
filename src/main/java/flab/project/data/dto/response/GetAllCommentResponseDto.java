11178af63f1de11c59bbdb62e5bad64086344770
eunbileeme <pask2202@gmail.com>
2023-07-15 00:53:17 +0900

    [chore] GetAllPostRequestDto 구조 완성 후 설계 예정

--- src/main/java/flab/project/data/dto/response/GetAllCommentResponseDto.java
+++ src/main/java/flab/project/data/dto/response/GetAllCommentResponseDto.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
-@Schema(description = "댓글을 가져오는 응답 Dto")
+@Schema(description = "댓글 목록을 가져오는 응답 Dto")
 public class GetAllCommentResponseDto {
     // Todo GetAllPostResponseDto 구조 완성 시, 설계 예정
 }
