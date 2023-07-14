ab206f2d3b10dcb64970c0ed52aa1761d8ee040d
eunbileeme <pask2202@gmail.com>
2023-07-15 00:39:42 +0900

    [chore] GetAllPostResponseDto 결정 후 설계 완료 예정

--- src/main/java/flab/project/data/dto/response/GetAllCommentResponseDto.java
+++ src/main/java/flab/project/data/dto/response/GetAllCommentResponseDto.java
@@ -6,5 +6,5 @@ import lombok.Getter;
 @Getter
 @Schema(description = "댓글을 가져오는 응답 Dto")
 public class GetAllCommentResponseDto {
-    // 대댓글을 구현하기 위한 상속 관계 설계 후, 구현 예정
+    // Todo GetAllPostResponseDto 구조 완성 시, 설계 예정
 }
