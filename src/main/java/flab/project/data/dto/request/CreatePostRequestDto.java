f8d1fa901904928aae30a8d018b9a080816469b2
eunbileeme <pask2202@gmail.com>
2023-07-15 00:38:29 +0900

    [chore] (상속을 구현하지 않을 경우) 주석 정리

--- src/main/java/flab/project/data/dto/request/CreatePostRequestDto.java
+++ src/main/java/flab/project/data/dto/request/CreatePostRequestDto.java
@@ -1,5 +1,6 @@
 package flab.project.data.dto.request;
 
+import flab.project.data.dto.model.BasicPostInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -7,9 +8,8 @@ import java.util.List;
 
 @Getter
 @Schema(description = "피드를 작성하는 Dto")
-public class CreatePostRequestDto {
+public class CreatePostRequestDto extends BasicPostInfo {
 
-    // PostAuthor 코드 추가 시, 수정 예정
     @Schema(example = "1")
     private long userId;
 
@@ -22,7 +22,7 @@ public class CreatePostRequestDto {
     @Schema(example = "오늘 깃허브에 알고리즘 관련 내용을 정리했습니다.", maxLength = 100)
     private String content;
 
-    // 이미지가 없을 경우, json으로 데이터가 직렬화 되는지 확인 - Controller 작성해서 확인
+    // Todo 이미지가 없을 경우, json으로 데이터가 직렬화 되는지 Controller 작성해서 확인
     @Schema(example = "https://imageUrl.url", nullable = false)
     private List<String> contentImgUrls;
 
