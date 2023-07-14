b95d149a2a615c7a454200f0e09544267b9dce8d
eunbileeme <pask2202@gmail.com>
2023-07-15 01:36:53 +0900

    [refactor] UpdatePostLikeRequestDto 설계

--- src/main/java/flab/project/data/dto/request/UpdatePostLikeRequestDto.java
+++ src/main/java/flab/project/data/dto/request/UpdatePostLikeRequestDto.java
@@ -1,5 +1,6 @@
 package flab.project.data.dto.request;
 
+import flab.project.data.enums.requestparam.UpdateLikeType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -10,10 +11,8 @@ public class UpdatePostLikeRequestDto {
     @Schema(example = "1")
     private long postId;
 
-    // enum - QueryString vs Body
-    @Schema(description = "좋아요 또는 좋아요 취소")
-    // 변수 이름 수정 예정
-    private boolean isLiked;
+    // Todo QueryString vs Body
+    private UpdateLikeType updateLikeType;
 
     @Schema(example = "1")
     private long userId;
