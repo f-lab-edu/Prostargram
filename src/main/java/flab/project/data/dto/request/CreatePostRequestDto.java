d603e200407ebcd1ebc14537f46a31ff6ce6a664
eunbileeme <pask2202@gmail.com>
2023-07-15 01:11:10 +0900

    [refactor] (상속을 구현할 경우) CreatePostRequestDto 설계

--- src/main/java/flab/project/data/dto/request/CreatePostRequestDto.java
+++ src/main/java/flab/project/data/dto/request/CreatePostRequestDto.java
@@ -10,24 +10,5 @@ import java.util.List;
 @Schema(description = "피드를 작성하는 Dto")
 public class CreatePostRequestDto extends BasicPostInfo {
 
-    @Schema(example = "1")
-    private long userId;
-
-    @Schema(example = "이은비")
-    private String userName;
-
-    @Schema(example = "https://profileImg.url")
-    private String profileImgUrl;
-
-    @Schema(example = "오늘 깃허브에 알고리즘 관련 내용을 정리했습니다.", maxLength = 100)
-    private String content;
-
-    // Todo 이미지가 없을 경우, json으로 데이터가 직렬화 되는지 Controller 작성해서 확인
-    @Schema(example = "https://imageUrl.url", nullable = false)
-    private List<String> contentImgUrls;
-
-    @Schema(example = "java")
-    private List<String> hashTags;
-
 }
 
