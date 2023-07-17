62daba2ca75911c8895a68c0be85b7dc8fa8a427
Ayaan <alsdnrdl001@gmail.com>
2023-07-17 16:28:00 +0900

    [refactor] Options 클래스는 추상클래스일 필요가 없으나 실수로 인해 추상클래스로 선언되어있었음. 수정

--- src/main/java/flab/project/data/dto/model/Options.java
+++ src/main/java/flab/project/data/dto/model/Options.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 @Schema(description = "유저의 설정 상태")
-public abstract class Options {
+public class Options {
     @Schema(description = "개정 공개/비공개 여부")
     private PublicScope publicScope;
 }
