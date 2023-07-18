86296b7d62b0db445c0792d61f8f010a03c817de
eunbileeme <pask2202@gmail.com>
2023-07-19 02:36:19 +0900

    [feat] GetEmailRequestDto 설계

---
+++ src/main/java/flab/project/data/dto/GetEmailRequestDto.java
@@ -0,0 +1,20 @@
+package flab.project.data.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "이메일 인증 Dto")
+public class GetEmailRequestDto {
+
+    @Schema(example = "example@example.com", nullable = false)
+    private final String email;
+
+    @Schema(example = "123456", nullable = false, description = "이메일로 전송한 인증 번호")
+    private final String authCode;
+
+    public GetEmailRequestDto(String email, String authCode) {
+        this.email = email;
+        this.authCode = authCode;
+    }
+}
