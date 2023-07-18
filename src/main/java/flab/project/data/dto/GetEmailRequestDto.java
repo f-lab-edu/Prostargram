a691c8cd0178d3fd9d72f63dab3176e6f8003c7c
eunbileeme <pask2202@gmail.com>
2023-07-19 02:37:50 +0900

    [refactor] @Schema attribute 순서 변경

--- src/main/java/flab/project/data/dto/GetEmailRequestDto.java
+++ src/main/java/flab/project/data/dto/GetEmailRequestDto.java
@@ -10,7 +10,7 @@ public class GetEmailRequestDto {
     @Schema(example = "example@example.com", nullable = false)
     private final String email;
 
-    @Schema(example = "123456", nullable = false, description = "이메일로 전송한 인증 번호")
+    @Schema(example = "123456", description = "이메일로 전송한 인증 번호", nullable = false)
     private final String authCode;
 
     public GetEmailRequestDto(String email, String authCode) {
