34807660d8ec3aff9d494c0312c2d92e3d131458
eunbileeme <pask2202@gmail.com>
2023-07-23 15:48:16 +0900

    [refactor] 좋아요 또는 좋아요 취소를 의미하는 enum LikeType을 필드로 추가

--- src/main/java/flab/project/data/dto/Comment.java
+++ src/main/java/flab/project/data/dto/Comment.java
@@ -1,5 +1,6 @@
 package flab.project.data.dto;
 
+import flab.project.data.enums.LikeType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -15,6 +16,9 @@ public class Comment {
 
     @Schema(example = "화이팅 합시다!")
     private String content;
+
+    @Schema(example = "좋아요 또는 좋아요 취소 enum")
+    private LikeType likeType;
 
     @Schema(example = "1", description = "대댓글이 존재할 경우, 가장 최상단의 댓글")
     private long rootId;
