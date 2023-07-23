7ee65237b0266425f3c53ae357c55985aae64347
eunbileeme <pask2202@gmail.com>
2023-07-23 15:47:57 +0900

    [refactor] BasicPost 불필요한 필드 수정 및 필요한 필드 추가

--- src/main/java/flab/project/data/dto/BasicPost.java
+++ src/main/java/flab/project/data/dto/BasicPost.java
@@ -1,5 +1,6 @@
 package flab.project.data.dto;
 
+import flab.project.data.enums.PostType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -7,18 +8,9 @@ import java.util.List;
 
 @Getter
 @Schema(description = "일반 피드 Dto")
-public class BasicPost {
+public class BasicPost extends BasePost {
 
-    @Schema(example = "1")
-    private long userId;
-
-    @Schema(example = "오늘 깃허브에 알고리즘 관련 내용을 정리했습니다.", maxLength = 100)
-    private String content;
-
-    @Schema(example = "java")
-    private List<String> hashTags;
-
-    @Schema(example = "일반")
-    private String type;
+    @Schema(example = "https://imageUrl.url", nullable = false)
+    private List<String> contentImgUrls;
 
 }
