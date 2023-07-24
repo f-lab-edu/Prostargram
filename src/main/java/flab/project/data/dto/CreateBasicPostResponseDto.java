08d9e8f0c32bfabfe80f864dc117d8d64c733fa6
eunbileeme <pask2202@gmail.com>
2023-07-24 17:23:37 +0900

    [refactor] 상속 -> 조합 관계

--- src/main/java/flab/project/data/dto/CreateBasicPostResponseDto.java
+++ src/main/java/flab/project/data/dto/CreateBasicPostResponseDto.java
@@ -1,42 +1,19 @@
 package flab.project.data.dto;
 
+import flab.project.data.dto.model.BasicUser;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @Schema(description = "일반 피드 작성 결과 Dto")
-public class CreateBasicPostResponseDto extends BasicPost {
+public class CreateBasicPostResponseDto {
 
-	@Schema(example = "1")
-	private final long postId;
+	private final BasicPost basicPost;
 
-	@Schema(example = "이은비")
-	private final String userName;
+	private final BasicUser basicUser;
 
-	@Schema(example = "https://profileImg.url")
-	private final String profileImgUrl;
-
-	@Schema(example = "https://imageUrl.url", nullable = false)
-	private final List<String> contentImgUrls;
-
-	@Schema(example = "1.4k", defaultValue = "0")
-	private final long likeCount;
-
-	@Schema(example = "14", defaultValue = "0")
-	private final long commentCount;
-
-	@Schema(example = "방금 전")
-	private final String creatTime;
-
-	public CreateBasicPostResponseDto(long postId, String userName, String profileImgUrl, List<String> contentImgUrls, long likeCount, long commentCount, String creatTime) {
-		this.postId = postId;
-		this.userName = userName;
-		this.profileImgUrl = profileImgUrl;
-		this.contentImgUrls = contentImgUrls;
-		this.likeCount = likeCount;
-		this.commentCount = commentCount;
-		this.creatTime = creatTime;
+	public CreateBasicPostResponseDto(BasicPost basicPost, BasicUser basicUser) {
+		this.basicPost = basicPost;
+		this.basicUser = basicUser;
 	}
 }
