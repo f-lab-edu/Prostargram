d61144d8133c7a7d630d8b4cdbaa3fd5b265fc38
eunbileeme <pask2202@gmail.com>
2023-07-24 17:30:10 +0900

    [refactor] 상속 -> 조합 관계

--- src/main/java/flab/project/data/dto/GetDetailPollPostResponseDto.java
+++ src/main/java/flab/project/data/dto/GetDetailPollPostResponseDto.java
@@ -7,28 +7,11 @@ import java.util.List;
 
 @Getter
 @Schema(description = "통계 피드 상세 보기")
-public class GetDetailPollPostResponseDto extends PollPost {
+public class GetDetailPollPostResponseDto {
 
-    @Schema(example = "1")
-    private final long postId;
+    private final PollPost pollPost;
 
-    @Schema(example = "이은비")
-    private final String userName;
-
-    @Schema(example = "https://profileImg.url")
-    private final String profileImgUrl;
-
-    @Schema(example = "1")
-    private final long optionId;
-
-    @Schema(example = "1.4k")
-    private final long likeCounts;
-
-    @Schema(example = "14")
-    private final long commentCounts;
-
-    @Schema(example = "방금 전")
-    private final String createTime;
+    private final User user;
 
     @Schema(description = "좋아요 순 베스트 댓글", example = "[1,2,3]")
     private final List<Comment> bestComments;
@@ -36,14 +19,9 @@ public class GetDetailPollPostResponseDto extends PollPost {
     @Schema(description = "최신 순 댓글", example = "[1,2,3,4,5]")
     private final List<Comment> recentComments;
 
-    public GetDetailPollPostResponseDto(long postId, String userName, String profileImgUrl, long optionId, long likeCounts, long commentCounts, String createTime, List<Comment> bestComments, List<Comment> recentComments) {
-        this.postId = postId;
-        this.userName = userName;
-        this.profileImgUrl = profileImgUrl;
-        this.optionId = optionId;
-        this.likeCounts = likeCounts;
-        this.commentCounts = commentCounts;
-        this.createTime = createTime;
+    public GetDetailPollPostResponseDto(PollPost pollPost, User user, List<Comment> bestComments, List<Comment> recentComments) {
+        this.pollPost = pollPost;
+        this.user = user;
         this.bestComments = bestComments;
         this.recentComments = recentComments;
     }
