252be1bba2527237a63609dcabc328e5ba1ec24c
eunbileeme <pask2202@gmail.com>
2023-07-24 17:31:01 +0900

    [refactor] 클래스 이름에 맞춰 필드 이름 변경

--- src/main/java/flab/project/data/dto/PatchPostLikeRequestDto.java
+++ src/main/java/flab/project/data/dto/PatchPostLikeRequestDto.java
@@ -1,5 +1,6 @@
 package flab.project.data.dto;
 
+import flab.project.data.enums.LikeType;
 import flab.project.data.enums.requestparam.UpdateLikeType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -8,10 +9,10 @@ import lombok.Getter;
 @Schema(description = "피드 좋아요 또는 좋아요 취소 Dto")
 public class PatchPostLikeRequestDto {
 
-    @Schema(example = "UPDATE_LIKE_REQUEST")
-    private final UpdateLikeType updateLikeType;
+    @Schema(example = "PATCH_LIKE_REQUEST")
+    private final LikeType likeType;
 
-    public PatchPostLikeRequestDto(UpdateLikeType updateLikeType) {
-        this.updateLikeType = updateLikeType;
+    public PatchPostLikeRequestDto(LikeType likeType) {
+        this.likeType = likeType;
     }
 }
