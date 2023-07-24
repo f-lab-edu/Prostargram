a881829137c0917cf32be127efdf07bd0a01d309
eunbileeme <pask2202@gmail.com>
2023-07-24 17:25:25 +0900

    [refactor] 상속 -> 조합 관계로 수정 및 클래스 이름 변경

--- src/main/java/flab/project/data/dto/CreateUser1ResponseDto.java
+++ src/main/java/flab/project/data/dto/CreateUser1ResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @Schema(description = "회원 가입 - 필수 정보 결과 Dto")
-public class CreateRequiredInfoResponseDto {
+public class CreateUser1ResponseDto {
 
     @Schema(example = "example@example.com")
     private final String email;
@@ -13,7 +13,7 @@ public class CreateRequiredInfoResponseDto {
     @Schema(example = "이은비")
     private final String userName;
 
-    public CreateRequiredInfoResponseDto(String email, String userName) {
+    public CreateUser1ResponseDto(String email, String userName) {
         this.email = email;
         this.userName = userName;
     }
