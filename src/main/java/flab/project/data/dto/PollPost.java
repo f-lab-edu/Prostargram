41b1e7c9a539288242be42a63962d831c0382e12
eunbileeme <pask2202@gmail.com>
2023-07-24 17:31:24 +0900

    [refactor] final 선언에 따른 protected로 접근 제어자 수정

--- src/main/java/flab/project/data/dto/PollPost.java
+++ src/main/java/flab/project/data/dto/PollPost.java
@@ -1,5 +1,6 @@
 package flab.project.data.dto;
 
+import flab.project.data.enums.PostType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -11,12 +12,18 @@ import java.util.List;
 public class PollPost extends BasePost {
 
     @Schema(example = "[2, 3, 4, 5]")
-    private List<Long> options;
+    private final List<Long> options;
 
     @Schema(example = "2023-07-01")
-    private LocalDate startDate;
+    private final LocalDate startDate;
 
     @Schema(example = "2023-07-14")
-    private LocalDate endDate;
+    private final LocalDate endDate;
 
+    public PollPost(long userId, long postId, String content, List<String> hashTags, PostType postType, long likeCount, long commentCount, String createTime, List<Long> options, LocalDate startDate, LocalDate endDate) {
+        super(userId, postId, content, hashTags, postType, likeCount, commentCount, createTime);
+        this.options = options;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
