f074b3121f3e8d408be26c4aba9525ae2356f7a4
eunbileeme <pask2202@gmail.com>
2023-07-24 17:23:22 +0900

    [refactor] final 선언에 따른 protected로 접근 제어자 수정

--- src/main/java/flab/project/data/dto/Comment.java
+++ src/main/java/flab/project/data/dto/Comment.java
@@ -9,18 +9,30 @@ import lombok.Getter;
 public class Comment {
 
     @Schema(example = "1")
-    private long postId;
+    protected long postId;
 
     @Schema(example = "1")
-    private long userId;
+    protected long userId;
 
-    @Schema(example = "화이팅 합시다!")
-    private String content;
-
-    @Schema(example = "좋아요 또는 좋아요 취소 enum")
-    private LikeType likeType;
+    @Schema(example = "1")
+    protected long commentId;
 
     @Schema(example = "1", description = "대댓글이 존재할 경우, 가장 최상단의 댓글")
-    private long rootId;
+    protected long rootId;
+
+    @Schema(example = "화이팅 합시다!")
+    protected String content;
+
+    @Schema(example = "좋아요 또는 좋아요 취소 enum")
+    protected LikeType likeType;
+
+    @Schema(example = "방금 전")
+    protected String createTime;
+
+    @Schema(example = "1400", defaultValue = "0")
+    protected long likeCount;
+
+    @Schema(example = "14", defaultValue = "0")
+    protected long commentCount;
 
 }
