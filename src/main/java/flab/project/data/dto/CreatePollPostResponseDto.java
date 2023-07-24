0e91062852d4eab379be54287f994b9c7b9bce95
eunbileeme <pask2202@gmail.com>
2023-07-24 17:24:51 +0900

    [refactor] 상속 -> 조합 관계

--- src/main/java/flab/project/data/dto/CreatePollPostResponseDto.java
+++ src/main/java/flab/project/data/dto/CreatePollPostResponseDto.java
@@ -1,37 +1,19 @@
 package flab.project.data.dto;
 
-import flab.project.data.dto.PollPost;
+import flab.project.data.dto.model.BasicUser;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 @Schema(description = "통계 피드 작성 결과 Dto")
-public class CreatePollPostResponseDto extends PollPost {
+public class CreatePollPostResponseDto{
 
-    @Schema(example = "1")
-    private final long postId;
+   private final PollPost pollPost;
 
-    @Schema(example = "이은비")
-    private final String userName;
+   private final BasicUser basicUser;
 
-    @Schema(example = "https://profileImg.url")
-    private final String profileImgUrl;
-
-    @Schema(example = "1.4k", defaultValue = "0")
-    private final long likeCount;
-
-    @Schema(example = "14", defaultValue = "0")
-    private final long commentCount;
-
-    @Schema(example = "방금 전")
-    private final String creatTime;
-
-    public CreatePollPostResponseDto(long postId, String userName, String profileImgUrl, long likeCount, long commentCount, String creatTime) {
-        this.postId = postId;
-        this.userName = userName;
-        this.profileImgUrl = profileImgUrl;
-        this.likeCount = likeCount;
-        this.commentCount = commentCount;
-        this.creatTime = creatTime;
+    public CreatePollPostResponseDto(PollPost pollPost, BasicUser basicUser) {
+        this.pollPost = pollPost;
+        this.basicUser = basicUser;
     }
 }
