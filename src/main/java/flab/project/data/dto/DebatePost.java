531b4ea2ef782f72220e3a145b511c89cc3784a0
eunbileeme <pask2202@gmail.com>
2023-07-24 17:26:08 +0900

    [refactor] final 선언에 따른 protected로 접근 제어자 수정

--- src/main/java/flab/project/data/dto/DebatePost.java
+++ src/main/java/flab/project/data/dto/DebatePost.java
@@ -1,13 +1,20 @@
 package flab.project.data.dto;
 
+import flab.project.data.enums.PostType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Schema(description = "토론 피드 Dto")
 public class DebatePost extends BasePost {
 
     @Schema(example = "1")
-    private long optionId;
+    private final long optionId;
 
+    public DebatePost(long userId, long postId, String content, List<String> hashTags, PostType postType, long likeCount, long commentCount, String createTime, long optionId) {
+        super(userId, postId, content, hashTags, postType, likeCount, commentCount, createTime);
+        this.optionId = optionId;
+    }
 }
