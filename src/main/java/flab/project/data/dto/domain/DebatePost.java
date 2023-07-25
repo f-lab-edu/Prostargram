7aadec1d91fe1b1679608547de366e53122006a9
eunbileeme <pask2202@gmail.com>
2023-07-26 01:22:29 +0900

    [refactor] DebatePost - Option 클래스 설계에 따른 필드 변경

---
+++ src/main/java/flab/project/data/dto/domain/DebatePost.java
@@ -0,0 +1,20 @@
+package flab.project.data.dto.domain;
+
+import flab.project.data.dto.Option;
+import flab.project.data.enums.PostType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Schema(description = "토론 피드 Dto")
+public class DebatePost extends BasePost {
+
+    private final List<Option> options;
+
+    public DebatePost(long userId, long postId, String content, List<String> hashTags, PostType postType, long likeCount, long commentCount, String createTime, long optionId, List<Option> options) {
+        super(userId, postId, content, hashTags, postType, likeCount, commentCount, createTime);
+        this.options = options;
+    }
+}
