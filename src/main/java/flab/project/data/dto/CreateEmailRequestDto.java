d7338aec090a88ae328c18b9c6fc852b44ef1036
eunbileeme <pask2202@gmail.com>
2023-07-28 03:02:09 +0900

    [refactor] 클래스 이름 및 @Schema description 내용 변경

--- src/main/java/flab/project/data/dto/CreateEmailRequestDto.java
+++ src/main/java/flab/project/data/dto/CreateEmailRequestDto.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
-@Schema(description = "이메일 인증코드 Dto")
-public class CreateEmailAuthCodeRequestDto {
+@Schema(description = "이메일 인증을 위한 이메일 생성 Dto")
+public class CreateEmailRequestDto {
 
     @Schema(example = "example@example.com", nullable = false)
     private final String email;
 
-    public CreateEmailAuthCodeRequestDto(String email) {
+    public CreateEmailRequestDto(String email) {
         this.email = email;
     }
 }
