3aee1c1a805830baa297676f76dae7da69ea816e
J-MU <alsdnrdl001@gmail.com>
2023-07-28 01:03:32 +0900

    [refactor] 프로enum 수정

--- src/main/java/flab/project/data/enums/requestparam/GetFollowsType.java
+++ src/main/java/flab/project/data/enums/requestparam/GetFollowsType.java
@@ -2,14 +2,6 @@ package flab.project.data.enums.requestparam;
 
 import lombok.Getter;
 
-@Getter
 public enum GetFollowsType {
-    FOLLOWERS("GET_FOLLOWERS_REQUEST"),
-    FOLLOWINGS("GET_FOLLOWINGS_REQUEST"),
-    ALL("GET_FOLLOWERS_AND_FOLLOWINGS_REQUEST");
-    private final String requestType;
-
-    private GetFollowsType(String requestType) {
-        this.requestType = requestType;
-    }
+    FOLLOWERS, FOLLOWINGS, ALL;
 }
--- src/main/java/flab/project/data/enums/requestparam/GetProfileRequestType.java
+++ src/main/java/flab/project/data/enums/requestparam/GetProfileRequestType.java
@@ -1,12 +1,5 @@
 package flab.project.data.enums.requestparam;
 
 public enum GetProfileRequestType {
-    GET("PROFILE_PAGE_REQUEST"),
-    UPDATE("UPDATE_PAGE_REQUEST");
-
-    private final String requestType;
-
-    private GetProfileRequestType(String requestType) {
-        this.requestType = requestType;
-    }
+    GET_PROFILE_PAGE,GET_PROFILE_UPDATE_PAGE;
 }
