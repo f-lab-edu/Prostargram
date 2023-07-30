4738d121ac788e98a6d2bd8eb545d4edf98cd7ee
J-MU <alsdnrdl001@gmail.com>
2023-07-31 03:17:37 +0900

    [feat] 팔로워/팔로잉 삭ì  기능 API Service 테스트 코드 구현

--- src/test/java/flab/project/service/FollowServiceUnitTest.java
+++ src/test/java/flab/project/service/FollowServiceUnitTest.java
@@ -1,6 +1,8 @@
 package flab.project.service;
 
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import flab.project.config.exception.InvalidUserInputException;
@@ -8,26 +10,56 @@ import flab.project.data.dto.model.Follows;
 import flab.project.mapper.FollowMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@WebMvcTest(controllers = FollowService.class)
+@ExtendWith(MockitoExtension.class)
 class FollowServiceUnitTest {
 
-    @Autowired
+    @InjectMocks
     FollowService followService;
 
-    @MockBean
+    @Mock
     FollowMapper followMapper;
 
-    @DisplayName("followRequestDto의 파라미터가 같은 값이 들어올 경우 InvalidUserInput Exception을 던진다.")
+    @DisplayName("팔로워/팔로잉을 추가할 때, followRequestDto의 파라미터가 같은 값이 들어올 경우 InvalidUserInput Exception을 던진다.")
     @Test
-    void followRequestDtoParameterIsNotSame(){
+    void followRequestDtoParameterIsNotSameInAddFollow(){
         //given
         Follows follows = new Follows(1L, 1L);
         // when then
         assertThatThrownBy(() -> followService.addFollow(follows))
             .isInstanceOf(InvalidUserInputException.class);
+    }
+    @DisplayName("팔로워/팔로잉을 추가할 때, followRequestDto의 파라미터가 같은 값이 아니면 정상 처리된다.")
+    @Test
+    void followRequestDtoParameterIsDifferentInAddFollow(){
+        //given
+        Follows follows = new Follows(1L, 2L);
+        // when then
+        assertThatCode(()-> followService.addFollow(follows))
+            .doesNotThrowAnyException();
+    }
+
+    @DisplayName("팔로워/팔로잉을 삭제할 때, followRequestDto의 파라미터가 같은 값이 들어올 경우 InvalidUserInput Exception을 던진다.")
+    @Test
+    void followRequestDtoParameterIsNotSameInDeleteFollow(){
+        //given
+        Follows follows = new Follows(1L, 1L);
+        // when then
+        assertThatThrownBy(() -> followService.deleteFollow(follows))
+            .isInstanceOf(InvalidUserInputException.class);
+    }
+
+    @DisplayName("팔로워/팔로잉을 삭제할 때, followRequestDto의 파라미터가 같은 값이 아니면 정상 처리된다.")
+    @Test
+    void followRequestDtoParameterIsDifferentInDeleteFollow(){
+        //given
+        Follows follows = new Follows(1L, 2L);
+        // when then
+        assertThatCode(()-> followService.deleteFollow(follows))
+            .doesNotThrowAnyException();
     }
 }
