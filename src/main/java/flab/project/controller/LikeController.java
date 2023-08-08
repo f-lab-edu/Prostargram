576069c8a9fbbd7f70096b21b922ad5151c54998
eunbileeme <pask2202@gmail.com>
2023-08-09 02:28:16 +0900

    [refactor] 입력값에 대한 validation 추가

--- src/main/java/flab/project/controller/LikeController.java
+++ src/main/java/flab/project/controller/LikeController.java
@@ -5,13 +5,16 @@ import flab.project.service.LikeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Validated
 public class LikeController {
 
     private final LikeService likeService;
@@ -19,8 +22,7 @@ public class LikeController {
     @Operation(summary = "게시물 좋아요 API")
     @Parameters({@Parameter(name = "postId", description = "게시물의 id", required = true)})
     @PostMapping(value = "/posts/{postId}/likes")
-    public SuccessResponse addPostLike(@PathVariable("postId") long postId, @PathVariable("userId") long userId) {
-        // Todo 로그인 기능(Spring Security)이 구현될 시, userId -> user.getId()로 변경
+    public SuccessResponse addPostLike(@PathVariable("postId") @Positive long postId, @PathVariable("userId") @Positive long userId) {
         return likeService.addPostLike(postId, userId);
     }
 }
