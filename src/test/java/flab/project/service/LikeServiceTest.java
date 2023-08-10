121240253beaa18735f68cc060ec9d19306f378c
eunbileeme <pask2202@gmail.com>
2023-08-11 04:10:36 +0900

    [refactor] LikeServiceTest - 올바른 Mapper 클래스를 호출하는지 확인하는 로직 추가

--- src/test/java/flab/project/service/LikeServiceTest.java
+++ src/test/java/flab/project/service/LikeServiceTest.java
@@ -9,8 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class LikeServiceTest {
@@ -21,42 +21,33 @@ public class LikeServiceTest {
     @Mock
     LikeMapper likeMapper;
 
-    @DisplayName("게시물에 좋아요를 추가할 때, postId 또는 userId의 값이 잘못된 경우(= 음수) InvalidUserInputException을 던진다.")
-    @Test
-    void invalidParameterInAddPostLike() {
-        // given
-        // postId is negative
-        long invalidPostId = -1L;
-        long userId = 1L;
-
-        // when then
-        assertThatThrownBy(() -> likeService.addPostLike(invalidPostId, userId)).isInstanceOf(InvalidUserInputException.class);
-
-        // given
-        // userId is negative
-        long postId = 1L;
-        long invalidUserId = -1L;
-
-        // when then
-        assertThatThrownBy(() -> likeService.addPostLike(postId, invalidUserId)).isInstanceOf(InvalidUserInputException.class);
-
-        // given
-        // both postId and userId are negative
-        long anotherInvalidPostId = -2L;
-        long anotherInvalidUserId = -2L;
-
-        // when then
-        assertThatThrownBy(() -> likeService.addPostLike(anotherInvalidPostId, anotherInvalidUserId)).isInstanceOf(InvalidUserInputException.class);
-    }
-
-    @DisplayName("게시물에 좋아요를 추가할 때, postId와 userId의 값이 올바르면 정상적으로 처리된다.")
+    @DisplayName("게시물에 좋아요를 추가할 때, 올바른 postId 및 userId가 들어온다면 두 값에 맞춰 알맞은 Mapper class가 호출이 된다.")
     @Test
     void validParamterInAddPostLike() {
-        // given
         long postId = 1L;
         long userId = 2L;
 
-        // when then
-        assertThatCode(() -> likeService.addPostLike(postId, userId)).doesNotThrowAnyException();
+        likeService.addPostLike(postId, userId);
+
+        verify(likeMapper).addPostLike(postId, userId);
+    }
+
+    @DisplayName("게시물에 좋아요를 추가할 때, postId 또는 userId의 값이 잘못된 경우(= 음수) InvalidUserInputException을 던진다.")
+    @Test
+    void invalidParameterInAddPostLike() {
+        long invalidPostId = -1L;
+        long userId = 1L;
+
+        assertThatThrownBy(() -> likeService.addPostLike(invalidPostId, userId)).isInstanceOf(InvalidUserInputException.class);
+
+        long postId = 1L;
+        long invalidUserId = -1L;
+
+        assertThatThrownBy(() -> likeService.addPostLike(postId, invalidUserId)).isInstanceOf(InvalidUserInputException.class);
+
+        long anotherInvalidPostId = -2L;
+        long anotherInvalidUserId = -2L;
+
+        assertThatThrownBy(() -> likeService.addPostLike(anotherInvalidPostId, anotherInvalidUserId)).isInstanceOf(InvalidUserInputException.class);
     }
 }
