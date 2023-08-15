0969555c7e3b507e83e14a34178843c550c178b8
eunbileeme <pask2202@gmail.com>
2023-08-15 17:59:43 +0900

    [refactor] ApiControllerAdvice - 클라이언트와 서버 예외 분리

--- src/main/java/flab/project/ApiControllerAdvice/ApiControllerAdvice.java
+++ src/main/java/flab/project/ApiControllerAdvice/ApiControllerAdvice.java
@@ -4,6 +4,7 @@ import flab.project.config.baseresponse.FailResponse;
 import flab.project.config.baseresponse.ResponseEnum;
 import flab.project.config.exception.InvalidUserInputException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiControllerAdvice {
 
+    /*
+    * 클라이언트에서 잘못 요청하여 발생한 예외를 위한 핸들링입니다.*/
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({InvalidUserInputException.class, RuntimeException.class})
-    public FailResponse exceptionResolve(Exception e) {
-        if (e instanceof InvalidUserInputException) {
-            return new FailResponse(ResponseEnum.INVALID_USER_INPUT);
-        } else {
-            return new FailResponse(ResponseEnum.SERVER_ERROR);
-        }
+    @ExceptionHandler({
+            InvalidUserInputException.class,
+            MethodArgumentNotValidException.class
+    })
+    public FailResponse exceptionResolveToInvalidUserInput(Exception e) {
+        e.printStackTrace();
+        System.out.println("ApiControllerAdvice.exceptionResolveToInvalidUserInput");
+        return new FailResponse(ResponseEnum.INVALID_USER_INPUT);
+    }
+
+    /*
+    * 서버 내부에서 예기치 못하게 발생한 예외를 위한 핸들링입니다.*/
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public FailResponse exceptionResolveToServerError(RuntimeException e) {
+        e.printStackTrace();
+        System.out.println("ApiControllerAdvice.exceptionResolveToServerError");
+        return new FailResponse(ResponseEnum.SERVER_ERROR);
     }
 }
