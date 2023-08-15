347d9e1e5bd2bce2ef80d9a60203f7098ff51b29
eunbileeme <pask2202@gmail.com>
2023-08-15 18:03:03 +0900

    [refactor] ResponseEnum 메세지 변경

--- src/main/java/flab/project/config/baseresponse/ResponseEnum.java
+++ src/main/java/flab/project/config/baseresponse/ResponseEnum.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 public enum ResponseEnum {
 
     SUCCESS(true, 1000, "요청에 성공하였습니다."),
-    INVALID_USER_INPUT(false, 4000, "잘못된 요청입니다."),
-    SERVER_ERROR(false, 5000, "서버 오류입니다. 잠시후 다시 시도하세요.");
+    INVALID_USER_INPUT(false, 4000, "올바르지 않은 경로의 요청입니다."),
+    SERVER_ERROR(false, 5000, "서버 오류입니다. 잠시 후 다시 시도하세요.");
 
     private final boolean isSuccess;
     private final int code;
