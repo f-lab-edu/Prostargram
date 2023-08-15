811cd2f64ea4aa22b13f3bf3a3e6cf6810981fe1
eunbileeme <pask2202@gmail.com>
2023-08-16 00:12:14 +0900

    [refactor] ResponseEnum 상수 추가

--- src/main/java/flab/project/config/baseresponse/ResponseEnum.java
+++ src/main/java/flab/project/config/baseresponse/ResponseEnum.java
@@ -5,14 +5,15 @@ import lombok.Getter;
 @Getter
 public enum ResponseEnum {
 
-    SUCCESS(true, 1000, "요청에 성공하였습니다.");
-
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+    INVALID_USER_INPUT(false, 4000, "올바르지 않은 경로의 요청입니다."),
+    SERVER_ERROR(false, 5000, "서버 오류입니다. 잠시 후 다시 시도하세요.");
 
     private final boolean isSuccess;
     private final int code;
     private final String message;
 
-    private ResponseEnum(boolean isSuccess, int code, String message) {
+    ResponseEnum(boolean isSuccess, int code, String message) {
         this.isSuccess = isSuccess;
         this.code = code;
         this.message = message;
