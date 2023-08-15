cb1eab2f634cf56459a3aff6a7e8e6cb0380ab01
eunbileeme <pask2202@gmail.com>
2023-08-15 18:01:23 +0900

    [refactor] InvalidUserInputException

--- src/main/java/flab/project/config/exception/InvalidUserInputException.java
+++ src/main/java/flab/project/config/exception/InvalidUserInputException.java
@@ -1,8 +1,7 @@
 package flab.project.config.exception;
 
 public class InvalidUserInputException extends RuntimeException {
-    public InvalidUserInputException() {
-    }
+    public InvalidUserInputException() {}
 
     public InvalidUserInputException(String message) {
         super(message);
