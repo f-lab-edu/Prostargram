f92d26a6244afef0bf00fde4121805e5baf1c5b2
eunbileeme <pask2202@gmail.com>
2023-08-15 18:02:40 +0900

    [refactor] LikeControllerTest 및 LikeServiceTest 변경, LikeServiceIntegrationTest 삭제

--- src/test/java/flab/project/controller/LikeControllerTest.java
+++ src/test/java/flab/project/controller/LikeControllerTest.java
@@ -3,6 +3,7 @@ package flab.project.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import flab.project.config.baseresponse.ResponseEnum;
 import flab.project.config.baseresponse.SuccessResponse;
+import flab.project.config.exception.InvalidUserInputException;
 import flab.project.service.LikeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,8 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,23 +31,49 @@ public class LikeControllerTest {
     @MockBean
     private LikeService likeService;
 
-    @DisplayName("게시물에 좋아요가 정상적으로 추가된다.")
+    @DisplayName("게시물에 좋아요를 추가할 때, postId 및 userId는 양수 값만 가능하다.")
     @Test
-    void addPostLike() throws Exception {
-        long postId = 1L;
-        long userId = 2L;
-        SuccessResponse response = new SuccessResponse();
+    void parameterOfLikeIsPostitive() throws Exception {
+        // given
+        long postId1 = 1L;
+        long userId1 = 2L;
 
-        when(likeService.addPostLike(anyLong(), anyLong())).thenReturn(response);
+        given(likeService.addPostLike(postId1, userId1)).willReturn(new SuccessResponse());
 
-        mockMvc.perform(post("/posts/{postId}/likes", "1", "2").contentType(MediaType.APPLICATION_JSON))
-                .andDo(print()).andExpect(status().isOk())
+        // when & then
+        mockMvc.perform(
+                post("/posts/{postId}/likes", postId1)
+                        .param("userId", String.valueOf(userId1))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.isSuccess").value(ResponseEnum.SUCCESS.isSuccess()))
                 .andExpect(jsonPath("$.code").value(ResponseEnum.SUCCESS.getCode()))
                 .andExpect(jsonPath("$.message").value(ResponseEnum.SUCCESS.getMessage()));
     }
 
-    /*
-    "좋아요를 추가할 때, postId 및 userId는 양수값 또는 필수값이다"에 해당하는 메서드 추가 예정
-     */
+    @DisplayName("게시물에 좋아요를 추가할 때, postId 및 userId는 음수 값일 수 없다.")
+    @Test
+    void parameterOfLikeIsNonPositive() throws Exception {
+        // given
+        long postId2 = -1L;
+        long userId2 = -2L;
+
+        /*
+        * 양수가 아닌 음수일 경우, 올바른 예외를 던지는지 확인하고 싶어서 작성한 코드입니다.
+        * 그런데, "왜 postId 또는 userId는 0보다 커야 한다." + "code : 400을 기대했지만 500이 반환된다."라는 오류가 뜨는지 모르겠습니다.
+        */
+
+        given(likeService.addPostLike(postId2, userId2)).willThrow(new InvalidUserInputException("postId 또는 userId는 0보다 커야 합니다."));
+
+        // when & then
+        mockMvc.perform(post("/posts/{postId}/likes", postId2)
+                        .param("userId", String.valueOf(userId2))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.isSuccess").value(ResponseEnum.INVALID_USER_INPUT.isSuccess()))
+                .andExpect(jsonPath("$.code").value(ResponseEnum.INVALID_USER_INPUT.getCode()))
+                .andExpect(jsonPath("$.message").value(ResponseEnum.INVALID_USER_INPUT.getMessage()));
+    }
 }
--- src/test/java/flab/project/service/LikeServiceTest.java
+++ src/test/java/flab/project/service/LikeServiceTest.java
@@ -10,7 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class LikeServiceTest {
@@ -21,33 +21,42 @@ public class LikeServiceTest {
     @Mock
     LikeMapper likeMapper;
 
-    @DisplayName("게시물에 좋아요를 추가할 때, 올바른 postId 및 userId가 들어온다면 두 값에 맞춰 알맞은 Mapper class가 호출이 된다.")
+    @DisplayName("존재하는 게시물 또는 존재하는 유저가 좋아요를 추가할 때, 정상적으로 게시물에 좋아요가 반영된다.")
     @Test
     void validParamterInAddPostLike() {
+        // given
         long postId = 1L;
         long userId = 2L;
 
+        // when
         likeService.addPostLike(postId, userId);
 
-        verify(likeMapper).addPostLike(postId, userId);
+        // then
+        then(likeMapper).should().addPostLike(postId, userId);
     }
 
-    @DisplayName("게시물에 좋아요를 추가할 때, postId 또는 userId의 값이 잘못된 경우(= 음수) InvalidUserInputException을 던진다.")
+    @DisplayName("존재하지 않는 게시물 또는 존재하지 않는 유저가 좋아요를 추가할 때, 게시물에 좋아요가 반영되지 않는다.")
     @Test
     void invalidParameterInAddPostLike() {
+        // given
         long invalidPostId = -1L;
         long userId = 1L;
 
+        // when & then
         assertThatThrownBy(() -> likeService.addPostLike(invalidPostId, userId)).isInstanceOf(InvalidUserInputException.class);
 
+        // given
         long postId = 1L;
         long invalidUserId = -1L;
 
+        // when & then
         assertThatThrownBy(() -> likeService.addPostLike(postId, invalidUserId)).isInstanceOf(InvalidUserInputException.class);
 
+        // given
         long anotherInvalidPostId = -2L;
         long anotherInvalidUserId = -2L;
 
+        // when & then
         assertThatThrownBy(() -> likeService.addPostLike(anotherInvalidPostId, anotherInvalidUserId)).isInstanceOf(InvalidUserInputException.class);
     }
 }
