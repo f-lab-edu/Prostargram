1d4f6fd2c8898ae439a6753e2983d311e3533292
Ayaan <alsdnrdl001@gmail.com>
2023-08-19 07:24:08 +0900

    [feat] getDomain메서드ìãrefactor 불 필요한 반복적인 로직을 static field로 ë¹추출해서 해결

--- src/main/java/flab/project/data/dto/model/SocialAccount.java
+++ src/main/java/flab/project/data/dto/model/SocialAccount.java
@@ -16,6 +16,9 @@ public class SocialAccount {
     private long userId;
     private String socialAccountUrl;
 
+    private static String urlRegex = "(?:https?://)?(?:www\\.)?([^/.]+)";
+    private static Pattern urlPatten = Pattern.compile(urlRegex);
+
     public SocialAccount(UpdateSocialAccountRequestDto updateSocialAccount) {
         this.userId = updateSocialAccount.getUserId();
         this.socialAccountUrl = updateSocialAccount.getSocialAccountUrl();
@@ -25,10 +28,8 @@ public class SocialAccount {
     //todo 이것도 프론트에서 한 번 Filtering을 해주는게 좋겠지?
     public String getDomain() {
         String linkUrl = getSocialAccountUrl();
-        String regex = "(?:https?://)?(?:www\\.)?([^/.]+)";
+        Matcher matcher = urlPatten.matcher(linkUrl);
 
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(linkUrl);
         if (matcher.find()) {
             String group = matcher.group(1);
             return group;
