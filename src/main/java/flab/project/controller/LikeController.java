268de3cc07a1c79d0bed722882b062e1671100e0
eunbileeme <pask2202@gmail.com>
2023-08-21 20:10:09 +0900

    [chore] LikeController - Todo "JWT 구현 이후 userId 입력 제거" 추가

--- src/main/java/flab/project/controller/LikeController.java
+++ src/main/java/flab/project/controller/LikeController.java
@@ -23,6 +23,7 @@ public class LikeController {
     @Operation(summary = "게시물 좋아요 API")
     @Parameters({@Parameter(name = "postId", description = "게시물의 id", required = true)})
     @PostMapping(value = "/posts/{postId}/likes")
+    // Todo JWT 구현 시, userId를 입력으로 받는 내용 제거 예정
     public SuccessResponse addPostLike(@PathVariable("postId") @Positive long postId, @RequestParam("userId") @Positive long userId) {
         return likeService.addPostLike(postId, userId);
     }
