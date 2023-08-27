74b2e7d7ba94581d275c9a99aabaf3e56d2b993c
Ayaan <alsdnrdl001@gmail.com>
2023-08-28 05:03:26 +0900

    [refactor] profileImage 테스트 코드 수정

--- src/test/java/flab/project/data/dto/file/ProfileImageTest.java
+++ src/test/java/flab/project/data/dto/file/ProfileImageTest.java
@@ -2,31 +2,46 @@ package flab.project.data.dto.file;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.mock.web.MockMultipartFile;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.multipart.MultipartFile;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.io.StringBufferInputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
 class ProfileImageTest {
 
-    @DisplayName("ProfileImage 생성자는 bucketName,fileName,objectMetadata를 초기화 하는 로직을 가지고 있다.")
+    @Mock
+    private MultipartFile multipartFile;
+
+    @DisplayName("프로필 이미지 생성자 테스트")
     @Test
-    void profileImageConstructorInitializeFields() {
+    void checkProfileImageConstructor() throws IOException {
         // given
-        MultipartFile multipartFile = new MockMultipartFile(
-                "profileImage",
-                "test.txt",
-                "text/plain",
-                "test file".getBytes()
-        );
+        String testContent = "test content";
+
+        given(multipartFile.getOriginalFilename()).willReturn("test.jpg");
+        given(multipartFile.getInputStream()).willReturn(new StringBufferInputStream(testContent));
 
         // when
         ProfileImage profileImage = new ProfileImage(1L, multipartFile);
+
+        String bucketName = profileImage.getBucketName();
         String fileName = profileImage.getFileName();
+        long contentLength = profileImage.getObjectMetadata().getContentLength();
 
         // then
-        assertThat(profileImage.getBucketName()).isEqualTo("profileimage/1");
-        assertThat(fileName.substring(fileName.lastIndexOf("."))).isEqualTo(".txt");
+        then(multipartFile).should().getInputStream();
+
+        assertThat(bucketName).isEqualTo("profileimage/1");
+        assertThat(fileName.substring(fileName.lastIndexOf("."))).isEqualTo(".jpg");
+        assertThat(contentLength).isEqualTo(testContent.length());
     }
 }
