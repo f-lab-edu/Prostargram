cd5ad23f761df454dd5189031eb0e9d745e87c66
eunbileeme <pask2202@gmail.com>
2023-09-06 02:34:51 +0900

    [refactor] @Mock -> @MockBean 수정

--- src/test/java/flab/project/controller/VoteControllerTest.java
+++ src/test/java/flab/project/controller/VoteControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,7 +31,7 @@ public class VoteControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Mock
+    @MockBean
     private VoteService voteService;
 
     @DisplayName("토론 게시물에 투표를 할 때, 유저는 오로지 하나의 선택지만 고를 수 있다.")
