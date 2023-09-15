64ac4bc6bc6c4a2f163acc3fc864493b7de7a9cd
J-MU <alsdnrdl001@gmail.com>
2023-09-15 11:15:07 +0900

    [refactor] 팔로우 Service 로직 수정

--- src/main/java/flab/project/service/FollowService.java
+++ src/main/java/flab/project/service/FollowService.java
@@ -6,11 +6,10 @@ import flab.project.data.dto.model.Follows;
 import flab.project.data.dto.model.User;
 import flab.project.data.enums.requestparam.GetFollowsType;
 import flab.project.mapper.FollowMapper;
+
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -20,54 +19,38 @@ public class FollowService {
     private final FollowMapper followMapper;
 
     public SuccessResponse<List<User>> getFollows(Long userId, GetFollowsType requestType) {
-        userIdValidation(userId);
+        validateUserIdPositive(userId);
 
-        List<User> result = followMapper.findAll(userId,requestType);
+        List<User> result = followMapper.findAll(userId, requestType);
 
         return new SuccessResponse<>(result);
     }
 
-    private void userIdValidation(Long userId) {
+    public SuccessResponse addFollow(Follows follows) {
+        validateFromUserIdAndToUserIdSame(follows);
+
+        followMapper.addFollow(follows);
+
+        return new SuccessResponse();
+    }
+
+    public SuccessResponse deleteFollow(Follows follows) {
+        validateFromUserIdAndToUserIdSame(follows);
+
+        followMapper.deleteFollow(follows);
+
+        return new SuccessResponse<>();
+    }
+
+    private void validateUserIdPositive(Long userId) {
         if (userId <= 0) {
             throw new InvalidUserInputException("userId는 양수여야 합니다.");
         }
     }
 
-    public SuccessResponse addFollow(Follows follows) {
-        try {
-            checkFromUserIdAndToUserIdSame(follows);
-
-            followMapper.addFollow(follows);
-
-            return new SuccessResponse();
-        } catch (InvalidUserInputException e) {
-            throw new InvalidUserInputException("fromUserId와 toUserId는 같을 수 없습니다.");
-        } catch (DuplicateKeyException e) {
-            throw new DuplicateKeyException("중복 요청으로 인해 나타난 에러입니다.");
-        } catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityViolationException("Foreign Key 문제로 발생한 에러입니다.");
-        } catch (Exception e) {
-            throw new RuntimeException("SERVER 에러 입니다.");
-        }
-    }
-
-    private void checkFromUserIdAndToUserIdSame(Follows follows) {
+    private void validateFromUserIdAndToUserIdSame(Follows follows) {
         if (follows.getFromUserId() == follows.getToUserId()) {
-            throw new InvalidUserInputException();
-        }
-    }
-
-    public SuccessResponse deleteFollow(Follows follows) {
-        try {
-            checkFromUserIdAndToUserIdSame(follows);
-
-            followMapper.deleteFollow(follows);
-
-            return new SuccessResponse<>();
-        } catch (InvalidUserInputException e) {
             throw new InvalidUserInputException("fromUserId와 toUserId는 같을 수 없습니다.");
-        } catch (Exception e) {
-            throw new RuntimeException();
         }
     }
 }
