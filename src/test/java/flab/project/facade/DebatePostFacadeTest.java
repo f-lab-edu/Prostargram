2d0107f5b07084df106b800175b0e8558c2156bc
J-MU <alsdnrdl001@gmail.com>
2023-09-24 22:08:43 +0900

    [feat] Facade Layer 단위 테스트 작성

---
+++ src/test/java/flab/project/facade/DebatePostFacadeTest.java
@@ -0,0 +1,68 @@
+package flab.project.facade;
+
+import flab.project.data.dto.model.AddDebatePostRequest;
+import flab.project.data.dto.model.AddPollPostRequest;
+import flab.project.service.DebatePostOptionService;
+import flab.project.service.PostHashTagService;
+import flab.project.service.PostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class DebatePostFacadeTest {
+
+    @InjectMocks
+    private DebatePostFacade debatePostFacade;
+    @Mock
+    PostService postService;
+    @Mock
+    PostHashTagService postHashTagService;
+    @Mock
+    DebatePostOptionService debatePostOptionService;
+
+    @DisplayName("DebatePost를 생성할 수 있다.")
+    @Test
+    void addPost() {
+        long userId = 1L;
+        AddDebatePostRequest validDebatePostRequest = AddDebatePostRequest.builder()
+                .content("게시물 내용입니다")
+                .hashTagNames(Set.of("#test1", "#test2"))
+                .optionContents(Set.of("content1", "content2"))
+                .build();
+
+        debatePostFacade.addPost(userId, validDebatePostRequest);
+        then(postService).should().addPost(userId, validDebatePostRequest);
+        then(postHashTagService).should().saveAll(anyLong(), eq(validDebatePostRequest.getHashTagNames()));
+        then(debatePostOptionService).should().savePostOptions(anyLong(), eq(validDebatePostRequest.getOptionContents()));
+    }
+
+    @DisplayName("AddDebatePostRequest가 아닌 다른 AddPostReuqest의 자식이 매개변수로 들어올 경우 RuntimeException을 던진다.")
+    @Test
+    void addPost_anotherChildOfAddPostRequest() {
+        long userId = 1L;
+        AddPollPostRequest pollPostRequest = AddPollPostRequest.builder()
+                .content("게시물 내용입니다")
+                .hashTagNames(Set.of("#test1", "#test2"))
+                .optionContents(Set.of("content1", "content2"))
+                .subject("test subject")
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .allowMultipleVotes(true)
+                .build();
+
+        assertThatThrownBy(() -> debatePostFacade.addPost(userId, pollPostRequest))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
---
+++ src/test/java/flab/project/facade/PollPostFacadeTest.java
@@ -0,0 +1,73 @@
+package flab.project.facade;
+
+import flab.project.data.dto.model.AddDebatePostRequest;
+import flab.project.data.dto.model.AddPollPostRequest;
+import flab.project.service.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class PollPostFacadeTest {
+
+    @InjectMocks
+    private PollPostFacade pollPostFacade;
+    @Mock
+    PostService postService;
+    @Mock
+    PostHashTagService postHashTagService;
+    @Mock
+    PollMetadataService pollMetadataService;
+    @Mock
+    PollPostOptionService pollPostOptionService;
+
+    @DisplayName("통계 게시물을 작성할 수 있다.")
+    @Test
+    void addPost() {
+        // given
+        long userId = 1L;
+        AddPollPostRequest validPollPostRequest = AddPollPostRequest.builder()
+                .content("게시물 내용입니다")
+                .hashTagNames(Set.of("#test1", "#test2"))
+                .optionContents(Set.of("content1", "content2"))
+                .subject("test subject")
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .allowMultipleVotes(true)
+                .build();
+
+        // when
+        pollPostFacade.addPost(userId, validPollPostRequest);
+
+        // then
+        then(postService).should().addPost(userId, validPollPostRequest);
+        then(postHashTagService).should().saveAll(anyLong(), eq(validPollPostRequest.getHashTagNames()));
+        then(pollMetadataService).should().addMetadata(validPollPostRequest);
+        then(pollPostOptionService).should().savePostOptions(anyLong(), eq(validPollPostRequest.getOptionContents()));
+    }
+
+    @DisplayName("AddPollPostRequest가 아닌 다른 AddPostReuqest의 자식이 매개변수로 들어올 경우 RuntimeException을 던진다.")
+    @Test
+    void addPost_anotherChildOfAddPostRequest() {
+        long userId = 1L;
+        AddDebatePostRequest debatePostRequest = AddDebatePostRequest.builder()
+                .content("게시물 내용입니다")
+                .hashTagNames(Set.of("#test1", "#test2"))
+                .optionContents(Set.of("content1", "content2"))
+                .build();
+
+        assertThatThrownBy(() -> pollPostFacade.addPost(userId, debatePostRequest))
+                .isInstanceOf(RuntimeException.class);
+    }
+}
