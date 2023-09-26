e4c56e5a81bfeeb623745c23a0958c860bcd4569
eunbileeme <pask2202@gmail.com>
2023-09-27 03:05:25 +0900

    [feat] Service, Mapper 구현 중

--- src/main/java/flab/project/mapper/CommentMapper.java
+++ src/main/java/flab/project/mapper/CommentMapper.java
@@ -7,5 +7,6 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CommentMapper {
 
-    Comment addComment(@Param("comment") Comment comment);
+    // Todo comment-mapper.xml에 대댓글 개수를 나타내는 commentCount(이름 변경 예정) 필드 추가 예정
+    void addComment(@Param("comment") Comment comment);
 }
--- src/main/java/flab/project/service/CommentService.java
+++ src/main/java/flab/project/service/CommentService.java
@@ -13,6 +13,11 @@ public class CommentService {
     private final CommentMapper commentMapper;
 
     public SuccessResponse<Comment> addComment (long postId, long userId, String content) {
+        // Todo 대댓글이 아닌 최상단 댓글을 작성할 경우, rootId를 어떻게 처리할지 세션에서 질문할 예정
+        // Todo 1) Controller에서 0인 rootId를 Service로 넘기는 방법
+        // Todo 2) rootId를 reference type으로 수정하여 null checking하는 방법
+        // Todo 그런데, 1)과 2)는 서버 입장에서 정확히 어떤 연유로 0 또는 null 값을 가지게 되었는지를 알 수 없어서 별로인 것 같음
+        // Todo 3) 최상단 댓글을 작성할 경우와 대댓글을 작성할 경우 엔드 포인트를 분리하는 방법은 어떨까?
         Comment comment = Comment.builder()
                 .postId(postId)
                 .userId(userId)
