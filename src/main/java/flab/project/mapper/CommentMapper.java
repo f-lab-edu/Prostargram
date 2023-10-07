41a7d016fc43a9a91b7fe4dbc310cf20a12c5fe5
eunbileeme <pask2202@gmail.com>
2023-10-07 18:36:20 +0900

    [chore] 주석 삭제

--- src/main/java/flab/project/mapper/CommentMapper.java
+++ src/main/java/flab/project/mapper/CommentMapper.java
@@ -7,6 +7,5 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CommentMapper {
 
-    // Todo comment-mapper.xml에 대댓글 개수를 나타내는 commentCount(이름 변경 예정) 필드 추가 예정
     void addComment(@Param("comment") Comment comment);
 }
