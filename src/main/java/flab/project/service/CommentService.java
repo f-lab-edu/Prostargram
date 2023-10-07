d687038afa3ddca8774e27ca355c1064745902aa
eunbileeme <pask2202@gmail.com>
2023-10-07 18:36:50 +0900

    [refactor] 검증 로직 추가

--- src/main/java/flab/project/service/CommentService.java
+++ src/main/java/flab/project/service/CommentService.java
@@ -1,11 +1,11 @@
 package flab.project.service;
 
+import flab.project.config.exception.InvalidUserInputException;
+import flab.project.config.exception.NotFoundException;
 import flab.project.data.dto.model.Comment;
 import flab.project.mapper.CommentMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
@@ -13,32 +13,54 @@ public class CommentService {
 
     private final CommentMapper commentMapper;
 
-    public Comment addComment (long postId, long userId, Long rootId, String content) {
-        Comment comment = createComment(postId, userId, content);
+    public Comment addComment(long postId, long userId, Long parentId, String content) {
+        validateComment(postId, userId, parentId);
+
+        Comment comment = Comment.builder()
+                .postId(postId)
+                .userId(userId)
+                .parentId(parentId)
+                .content(content)
+                .build();
+
+        if (comment == null) {
+            throw new NotFoundException("comment is not found.");
+        }
 
         commentMapper.addComment(comment);
 
-        setRootId(comment, rootId);
+        setRootId(comment, parentId);
 
         return comment;
     }
 
-    private Comment createComment(long postId, long userId, String content) {
-        LocalDateTime todayTime = LocalDateTime.now();
-
-        return Comment.builder()
-                .postId(postId)
-                .userId(userId)
-                .content(content)
-                .createdAt(todayTime)
-                .build();
+    private void validateComment(long postId, long userId, Long parentId) {
+        // Todo userId는 추후 삭제 예정이므로 합쳐서 작성
+        validatePostIdAndUserId(postId, userId);
+        validateParentId(parentId);
     }
 
-    // Todo 근데 rootId 값이 null일 경우, rootId 값이 commentId 값으로 세팅되는 걸 Swagger UI상에서 확인할 수가 없는데..
-    private void setRootId(Comment comment, Long rootId) {
-        if (rootId == null) {
-            rootId = comment.getCommentId();
+    private void validatePostIdAndUserId(long postId, long userId) {
+        if (postId <= 0) {
+            throw new InvalidUserInputException("Invalid postId.");
         }
-        comment.setRootId(rootId);
+
+        if (userId <= 0) {
+            throw new InvalidUserInputException("Invalid userId.");
+        }
+    }
+
+    private void validateParentId(Long parentId) {
+        if (parentId <= 0) {
+            throw new InvalidUserInputException("Invalid parentId.");
+        }
+    }
+
+    // Todo (CommentController 26L) parentId에 commentId를 넣지 않고, null을 그대로 넣는 방식으로 회의 후 수정 예정 / builder에 rootId 추가
+    private void setRootId(Comment comment, Long parentId) {
+        if (parentId == null) {
+            parentId = comment.getCommentId();
+        }
+        comment.setParentId(parentId);
     }
 }
