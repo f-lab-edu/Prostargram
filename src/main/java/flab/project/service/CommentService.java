bbd062892b60d19f0c9ee22084f30ba4af7d4c54
eunbileeme <pask2202@gmail.com>
2023-10-10 03:22:33 +0900

    [refactor] 코드 정리

--- src/main/java/flab/project/service/CommentService.java
+++ src/main/java/flab/project/service/CommentService.java
@@ -1,7 +1,6 @@
 package flab.project.service;
 
 import flab.project.config.exception.InvalidUserInputException;
-import flab.project.config.exception.NotFoundException;
 import flab.project.data.dto.model.Comment;
 import flab.project.mapper.CommentMapper;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +22,12 @@ public class CommentService {
                 .content(content)
                 .build();
 
-        if (comment == null) {
-            throw new NotFoundException("comment is not found.");
-        }
+        // Todo comment가 null이라는 건 너무 추상적으로 이해가 되는데.., 댓글일 경우 null을 가지는 parentId + Primitive Type인 postId 및 userId를 제외하고는 content에 null이 담겼을 경우밖에 없을 것 같은데?
+        // Todo 이건 controller에서 해줄 수 있는 검증인데, service에서도 존재하지 않는 댓글이라는 어색한 context로 검증을 해줘야 할까?
 
         commentMapper.addComment(comment);
 
-        setRootId(comment, parentId);
+        // setRootId(comment, parentId);
 
         return comment;
     }
@@ -51,16 +49,16 @@ public class CommentService {
     }
 
     private void validateParentId(Long parentId) {
-        if (parentId <= 0) {
+        if (parentId != null && parentId <= 0) {
             throw new InvalidUserInputException("Invalid parentId.");
         }
     }
 
-    // Todo (CommentController 26L) parentId에 commentId를 넣지 않고, null을 그대로 넣는 방식으로 회의 후 수정 예정 / builder에 rootId 추가
-    private void setRootId(Comment comment, Long parentId) {
-        if (parentId == null) {
-            parentId = comment.getCommentId();
-        }
-        comment.setParentId(parentId);
-    }
+    // Todo (CommentController 26L) parentId에 commentId를 넣지 않고, null을 그대로 넣는 방식으로 회의 후 수정 예정 / builder에 parentId 추가
+    // private void setRootId(Comment comment, Long parentId) {
+    //    if (parentId == null) {
+    //        parentId = comment.getCommentId();
+    //    }
+    //    comment.setParentId(parentId);
+    // }
 }
