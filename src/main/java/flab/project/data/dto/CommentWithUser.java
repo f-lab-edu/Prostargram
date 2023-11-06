42f6a11c19aced28e91e1620705f6512309944a7
eunbileeme <pask2202@gmail.com>
2023-11-07 00:56:32 +0900

    [refactor] @Getter 추가 및 매개변수 생성자 삭제

--- src/main/java/flab/project/data/dto/CommentWithUser.java
+++ src/main/java/flab/project/data/dto/CommentWithUser.java
@@ -2,16 +2,13 @@ package flab.project.data.dto;
 
 import flab.project.data.dto.model.BasicUser;
 import flab.project.data.dto.model.Comment;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 public class CommentWithUser {
     private Comment comment;
 
     private BasicUser basicUser;
-
-    public CommentWithUser(Comment comment, BasicUser basicUser) {
-        this.comment = comment;
-        this.basicUser = basicUser;
-    }
 }
