95800158afa405dbd685d9ed61d9061dbc0145fe
eunbileeme <pask2202@gmail.com>
2023-11-07 00:51:24 +0900

    [chore] 필드 순서 정리

--- src/main/java/flab/project/data/dto/model/Comment.java
+++ src/main/java/flab/project/data/dto/model/Comment.java
@@ -13,13 +13,13 @@ import java.time.LocalDateTime;
 public class Comment {
 
     @Schema(example = "1")
+    private long commentId;
+
+    @Schema(example = "1")
     private long postId;
 
     @Schema(example = "1")
     private long userId;
-
-    @Schema(example = "1")
-    private long commentId;
 
     @Schema(description = "대댓글이 존재할 경우, 가장 최상단의 댓글", example = "1", nullable = true)
     @Setter
