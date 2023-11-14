ffeee144c3ca10c7a09d51c1f907d03d5fd4d676
eunbileeme <pask2202@gmail.com>
2023-11-14 20:52:18 +0900

    [chore] 주석 수정 및 코드 정리

--- src/main/java/flab/project/controller/DuplicationController.java
+++ src/main/java/flab/project/controller/DuplicationController.java
@@ -22,10 +22,8 @@ public class DuplicationController {
     private final DuplicationService duplicationService;
 
     // Todo 욕설 및 비속어 필터링은 커스텀 어노테이션을 만들 예정
-    // Todo 영어/한글/숫자/_ 및 온점 사용 가능 필터링은 추가 예정
     @Operation(summary = "닉네임 중복 검증 API")
-    @Parameters({
-            @Parameter(name = "userName", description = "유저의 닉네임", in = ParameterIn.QUERY, required = true)})
+    @Parameters({@Parameter(name = "userName", description = "유저의 닉네임", in = ParameterIn.QUERY, required = true)})
     @PostMapping(value = "/users")
     public SuccessResponse verifyDuplicateUserName(@RequestParam("userName") @NotBlank @Size(min = 1, max = 16) String userName) {
         String userNameToken = duplicationService.verifyDuplicateUserName(userName);
