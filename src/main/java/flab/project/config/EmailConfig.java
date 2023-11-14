5398cff306ac3e416fa74aa0d18b7cbef5b34219
eunbileeme <pask2202@gmail.com>
2023-11-14 22:16:23 +0900

    [refactor] email.properties -> application.yml 통합

--- src/main/java/flab/project/config/EmailConfig.java
+++ src/main/java/flab/project/config/EmailConfig.java
@@ -3,45 +3,40 @@ package flab.project.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:email.properties")
 public class EmailConfig {
 
-    @Value("${mail.smtp.port}")
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
     private int port;
 
-    @Value("${mail.smtp.socketFactory.port}")
-    private int socketPort;
-
-    @Value("${mail.smtp.auth}")
-    private boolean auth;
-
-    @Value("${mail.smtp.starttls.enable}")
-    private boolean starttls;
-
-    @Value("${mail.smtp.starttls.required}")
-    private boolean startlls_required;
-
-    @Value("${mail.smtp.socketFactory.fallback}")
-    private boolean fallback;
-
-    @Value("${AdminMail.id}")
+    @Value("${spring.mail.username}")
     private String id;
 
-    @Value("${AdminMail.password}")
+    @Value("${spring.mail.password}")
     private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean starttls;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.required}")
+    private boolean startlls_required;
 
     @Bean
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
-        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setHost(host);
         javaMailSender.setUsername(id);
         javaMailSender.setPassword(password);
         javaMailSender.setPort(port);
@@ -54,11 +49,9 @@ public class EmailConfig {
     private Properties getMailProperties() {
         Properties properties = new Properties();
 
-        properties.put("mail.smtp.socketFactory.port", socketPort);
         properties.put("mail.smtp.auth", auth);
         properties.put("mail.smtp.starttls.enable", starttls);
         properties.put("mail.smtp.starttls.required", startlls_required);
-        properties.put("mail.smtp.socketFactory.fallback", fallback);
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         return properties;
