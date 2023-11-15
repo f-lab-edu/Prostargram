e95b73ea08ec24796d3578530a3e970788d4cf91
eunbileeme <pask2202@gmail.com>
2023-11-15 13:50:09 +0900

    [feat] Reids 관련 파일 추가

---
+++ src/main/java/flab/project/utils/RedisUtil.java
@@ -0,0 +1,34 @@
+package flab.project.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class RedisUtil {
+
+    private final StringRedisTemplate template;
+
+    public String get(String key) {
+        ValueOperations<String, String> valueOperations = template.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public boolean exist(String key) {
+        return template.hasKey(key);
+    }
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = template.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+    public void delete(String key) {
+        template.delete(key);
+    }
+}
