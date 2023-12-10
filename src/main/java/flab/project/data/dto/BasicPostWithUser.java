9d4c501bc26ed36024593775af75f4e9bce188a7
eunbileeme <pask2202@gmail.com>
2023-12-11 00:44:02 +0900

    [refactor] 값 할당을 위해 @NoArgs, @AllArgs .. 어노테이션 추가

--- src/main/java/flab/project/data/dto/BasicPostWithUser.java
+++ src/main/java/flab/project/data/dto/BasicPostWithUser.java
@@ -2,11 +2,13 @@ package flab.project.data.dto;
 
 import flab.project.data.dto.model.BasicPost;
 import flab.project.data.dto.model.BasicUser;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class BasicPostWithUser {
 
     private BasicPost basicPost;
--- src/main/java/flab/project/data/dto/model/BasicPost.java
+++ src/main/java/flab/project/data/dto/model/BasicPost.java
@@ -1,11 +1,15 @@
 package flab.project.data.dto.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "일반 게시물 Dto")
 public class BasicPost extends BasePost {
 
