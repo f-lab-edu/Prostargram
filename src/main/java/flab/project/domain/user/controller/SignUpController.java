ec5a7d943113f8b8ebd730ef78a32432fa124149
eunbileeme <pask2202@gmail.com>
2024-08-11 23:48:46 +0900

    [feat] 회원가입 2단계 API 구현

--- src/main/java/flab/project/domain/user/controller/SignUpController.java
+++ src/main/java/flab/project/domain/user/controller/SignUpController.java
@@ -1,7 +1,11 @@
 package flab.project.domain.user.controller;
 
 import flab.project.config.baseresponse.SuccessResponse;
+import flab.project.domain.user.facade.InterestFacade;
+import flab.project.domain.user.facade.SocialAccountFacade;
+import flab.project.domain.user.model.AddInterest;
 import flab.project.domain.user.model.SignUp;
+import flab.project.domain.user.model.UpdateSocialAccountRequestDto;
 import flab.project.domain.user.service.SignUpService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -9,12 +13,15 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Validated
 @RequiredArgsConstructor
@@ -22,6 +29,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignUpController {
 
     private final SignUpService signUpService;
+    private final SocialAccountFacade socialAccountFacade;
+    private final InterestFacade interestFacade;
 
     // Todo 비밀번호 필터링에 대한 커스텀 어노테이션 추가 예정
     @Operation(summary = "회원가입 API - 1단계")
@@ -33,6 +42,39 @@ public class SignUpController {
                                               @RequestParam @NotBlank String emailToken,
                                               @RequestParam @NotBlank String userNameToken) {
         signUpService.addUser(signUp, emailToken, userNameToken);
+
+        return new SuccessResponse<>();
+    }
+
+    @Operation(summary = "회원가입 API - 2단계")
+    @Parameters({
+            @Parameter(name = "userId", description = "유저의 id", in = ParameterIn.PATH, required = true),
+    })
+    @PostMapping(value = "/users/{userId}/social-accounts-and-interests")
+    public SuccessResponse addSocialAccountAndInterest(@PathVariable("userId") @Positive long userId,
+                                                       @RequestBody @Valid @Size(max = 3) Optional<List<UpdateSocialAccountRequestDto>> socialAccounts,
+                                                       @RequestBody @Valid Optional<List<String>> recommendInterests,
+                                                       @RequestBody @Valid @Size(max = 10) Optional<List<AddInterest>> customInterests) {
+
+        socialAccounts.ifPresent(socialAccountList -> {
+            for (UpdateSocialAccountRequestDto socialAccount : socialAccountList) {
+                socialAccount.convertEscapeCharacter();
+                socialAccountFacade.addSocialAccount(socialAccount);
+            }
+        });
+
+        recommendInterests.ifPresent(interestList -> {
+            for (String interest : interestList) {
+                AddInterest addInterest = new AddInterest(userId, interest);
+                interestFacade.addInterest(addInterest);
+            }
+        });
+
+        customInterests.ifPresent(interestList -> {
+            for (AddInterest interest : interestList) {
+                interestFacade.addInterest(interest);
+            }
+        });
 
         return new SuccessResponse<>();
     }
