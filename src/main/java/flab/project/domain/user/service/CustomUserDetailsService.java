c12e770a9907c94f69a9f82c209642e9275ecb17
eunbileeme <pask2202@gmail.com>
2024-09-18 17:58:54 +0900

    [chore] 코드 정리

--- src/main/java/flab/project/domain/user/service/CustomUserDetailsService.java
+++ src/main/java/flab/project/domain/user/service/CustomUserDetailsService.java
@@ -24,12 +24,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserForAuth user = authenticationMapper.getUser(username);
+
         // todo EncodedPassword가 어떻게 나올 수 있지..?
         String encodedPassword = passwordEncoder.encode(user.getPassword());
-        System.out.println("encodedPassword = " + encodedPassword);
 
-        List<GrantedAuthority> grantedAuthorities = List.of(new SimpleGrantedAuthority(user.getUserType().name()));
         // todo 왜 권한정보를 List로 받아야 하게 만들었을까..? 권한을 계층 구조로 만들어야하나..?
+        List<GrantedAuthority> grantedAuthorities = List.of(new SimpleGrantedAuthority(user.getUserType().name()));
 
         return new User(String.valueOf(user.getUserId()), user.getPassword(), grantedAuthorities);
     }
