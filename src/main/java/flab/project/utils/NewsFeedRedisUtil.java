f9aeecd2a6467cb839a045ce904b5a008349ad35
MIN UK JUNG <alsdnrdl001@gmail.com>
2024-09-27 21:34:17 +0900

    [refactor] FanOut Server를 위한 코드 제거 (#160)

--- src/main/java/flab/project/utils/NewsFeedRedisUtil.java
+++ src/main/java/flab/project/utils/NewsFeedRedisUtil.java
@@ -2,9 +2,7 @@ package flab.project.utils;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,15 +21,6 @@ public class NewsFeedRedisUtil {
 
     public boolean exists(Long key) {
         return newsFeedRedisTemplate.hasKey(key);
-    }
-
-    public void addForMultipleUsers(List<Long> userIds, long postId) {
-        newsFeedRedisTemplate.executePipelined((RedisCallback<Object>) connection -> {
-            for (long userId : userIds) {
-                newsFeedRedisTemplate.opsForList().leftPush(userId, postId);
-            }
-            return null;
-        });
     }
 
     public void delete(Long key) {
