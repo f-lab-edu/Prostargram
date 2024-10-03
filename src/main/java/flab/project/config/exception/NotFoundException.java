a856c61628503417a33cc9b12cd3ae4d8850c251
eunbileeme <pask2202@gmail.com>
2024-10-04 00:16:42 +0900

    [refactor] NotFoundException 기본 생성자 삭제

--- src/main/java/flab/project/config/exception/NotFoundException.java
+++ src/main/java/flab/project/config/exception/NotFoundException.java
@@ -2,9 +2,6 @@ package flab.project.config.exception;
 
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException() {
-    }
-
     public NotFoundException(String message) {
         super(message);
     }
