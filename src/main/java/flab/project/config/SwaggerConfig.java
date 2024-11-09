69d3e96044a7cd22ae23f32f6625c82509fea1fa
J-MU <alsdnrdl001@gmail.com>
2024-11-09 13:02:08 +0900

    temp

--- src/main/java/flab/project/config/SwaggerConfig.java
+++ src/main/java/flab/project/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package flab.project.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,13 +12,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://www.prostargram.shop", description = "개발 서버"),
+                @Server(url = "http://localhost:8080", description = "로컬 서버")
+        })
 @Configuration
 public class SwaggerConfig {
 
-        @Bean
-        public OpenAPI openAPI() {
-                return new OpenAPI()
-                        .info(new Info()
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
                         .title("Prostargram API 문서")
                         .description("개발과 관련된 일상을 공유하는 소셜 네트워크 서비스입니다.")
                         .version("1.0.0"))
@@ -28,7 +35,7 @@ public class SwaggerConfig {
                                 .in(In.HEADER)
                                 .name(HttpHeaders.AUTHORIZATION)
                         ))
-                        .addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION));
-        }
+                .addSecurityItem(new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION));
+    }
 
 }
