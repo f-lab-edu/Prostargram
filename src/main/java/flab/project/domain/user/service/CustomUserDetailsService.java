a8d517a25224221437f679f06a1c48342c29ae7e
J-MU <alsdnrdl001@gmail.com>
2025-01-22 15:51:22 +0900

    [Fix] 아이디/패스워드가 틀렸을 때, "아이디 혹은 비밀번호가 틀렸습니다."응답

--- src/main/java/flab/project/domain/user/service/CustomUserDetailsService.java
+++ src/main/java/flab/project/domain/user/service/CustomUserDetailsService.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,7 +19,6 @@ import java.util.List;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final AuthenticationMapper authenticationMapper;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
