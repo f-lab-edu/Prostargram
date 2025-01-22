081c7fc20003827e7f28ab8b60487c614b84232f
MIN UK JUNG <alsdnrdl001@gmail.com>
2025-01-22 15:53:19 +0900

    [Fix] 아이디/패스워드가 틀렸을 때 응답 포맷 변경 (#253)
    * [Fix] 아이디/패스워드가 틀렸을 때, "아이디 혹은 비밀번호가 틀렸습니다."응답

--- src/main/java/flab/project/domain/user/service/CustomUserDetailsService.java
+++ src/main/java/flab/project/domain/user/service/CustomUserDetailsService.java
@@ -3,13 +3,13 @@ package flab.project.domain.user.service;
 import flab.project.domain.user.model.UserForAuth;
 import flab.project.domain.user.mapper.AuthenticationMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,16 +19,15 @@ import java.util.List;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final AuthenticationMapper authenticationMapper;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserForAuth user = authenticationMapper.getUser(username);
 
-        // todo EncodedPassword가 어떻게 나올 수 있지..?
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        if (user == null) {
+            throw new BadCredentialsException("아이디 혹은 패스워드가 틀렸습니다.");
+        }
 
-        // todo 왜 권한정보를 List로 받아야 하게 만들었을까..? 권한을 계층 구조로 만들어야하나..?
         List<GrantedAuthority> grantedAuthorities = List.of(new SimpleGrantedAuthority(user.getUserType().name()));
 
         return new User(String.valueOf(user.getUserId()), user.getPassword(), grantedAuthorities);
